2feb68c5058951d07d8167b6bf19f879e8299cff
Paul Onoprienko <paulonoprienko96@gmail.com>
2026-01-02 02:45:02 +0300

    fixing getDistance function

--- map.js
+++ map.js
@@ -135,7 +135,7 @@ const handlePointerEnd = (e) => {
 };
 
 const getDistance = (p1, p2) => {
-  return Math.sqrt(Math.pow(p2.x - p1.y, 2) + Math.pow(p2.y - p1.y, 2));
+  return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 };
 
 svg.addEventListener("pointerdown", (e) => {
